170f9d8f1729f46b69f61614bc879c142cc21164
tokyo <tokyolabs@protonmail.com>
2024-01-08 17:26:51 -0500

    set up game

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill("purple")
