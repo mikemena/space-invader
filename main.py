85a28aa5be669e8ddfab9c4324321f690c8357df
tokyo <tokyolabs@protonmail.com>
2024-01-09 17:25:04 -0500

    set screen movement limits

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-# spaceship
+# spaceship variables
 spaceship_img = pygame.image.load("images/spaceship.png")
 spaceship_x = 608
 spaceship_y = 650
 spaceship_x_change = 0
 
 
+# spaceship function
 def spaceship(x, y):
     screen.blit(spaceship_img, (x, y))
 
@@ -21,22 +22,34 @@ def spaceship(x, y):
 icon = pygame.image.load("images/ufo.png")
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Space Invaders")
+
+# main game loop
 while running:
     screen.fill((72, 60, 148))
     # spaceship_x += 0.1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # handle press arrow key events
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 spaceship_x_change = -1.5
             if event.key == pygame.K_RIGHT:
                 spaceship_x_change = 1.5
+        # handle key release
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 spaceship_x_change = 0
 
+    # handle spaceship movement
     spaceship_x += spaceship_x_change
     spaceship(spaceship_x, spaceship_y)
 
+    # keep the spaceship in the screen
+    if spaceship_x <= 5:
+        spaceship_x = 5
+    elif spaceship_x >= 1210:
+        spaceship_x = 1210
+
+    # update screen
     pygame.display.update()
